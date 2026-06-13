1b2bf971d710c1cb41e1eb6445f53415e42f84bb
Udan Fernando <udanfernando2006@gmail.com>
2026-06-13 15:25:21 +0530

    feat: Initialize Maven project structure with necessary configuration files and application setup

---
+++ api/src/test/java/io/github/udanfernando/vestige/VestigeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.udanfernando.vestige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
